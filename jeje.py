9fefb0a6305db3dbf700029eb67a4c869a2d9782
rady y <rady.y@passerellesnumeriques.org>
2020-12-02 11:13:50 +0700

    Add files via upload

---
+++ jeje.py
@@ -0,0 +1,16 @@
+books = [
+    {
+        "name": "Math",
+        "price": 3000
+    },
+    {
+        "name": "Chemistry",
+        "price": 5000
+    },
+    {
+        "name": "History",
+        "price": 10000
+    }
+]
+
+# Which book are cheaper
---
+++ mala.py
@@ -0,0 +1,13 @@
+nameList = {
+    "student1": { "name": "rady", "age": 25 },
+    "student2": { "name": "kannha", "age": 16 },
+    "student3": { "name": "Pheak", "age": 20 },
+    "student4": { "name": "Thida", "age": 10 }
+}
+
+# 1. Loop to display all students from list
+
+# 2. Find oldest age in this list
+
+# 3. Find youngest age in this list
+
